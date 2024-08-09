1ce8bc1c44a25a9b54ec31b3a05078a66f9c3d8e
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-08-09 14:40:36 +0200

    Update time.ts

--- src/decorators/time.ts
+++ src/decorators/time.ts
@@ -2,7 +2,7 @@ export function time(target, descriptor) {
   console.log({ descriptor }, { target })
   if (descriptor.kind !== 'method') throw new Error(`expected ${descriptor.name} to be a method`)
   descriptor.addInitializer(function () {
-    pubsub.subscribe('time-interval', (value) => {
+    globalThis.pubsub.subscribe('time-interval', (value) => {
       this.onTimeChange?.(value)
     })
   })
