7ca1e1a876ff7592442903379d4e435bd9970bc0
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-09-23 12:22:39 +0200

    Add Itoken

---
+++ src/interfaces/i-token.ts
@@ -0,0 +1,9 @@
+export interface IToken {
+  mint?(to: string, amount: bigint): void
+  burn?(from: string, amount: bigint): void
+  transfer?(from: string, to: string, amount: bigint): void
+  balance?(): bigint
+  balanceOf?(address: string): bigint
+  setApproval?(operator: string, amount: bigint): void
+  approved?(owner: string, operator: string, amount: bigint): boolean
+}
